0ccf4ffa6becf07290f7994aa7f1a38ed99e1df4
Sam Johnson <sam@durosoft.com>
2025-05-27 00:35:41 -0400

    vec reading and writing

--- src/io.rs
+++ src/io.rs
@@ -64,3 +64,29 @@ impl<W: std::io::Write> Write for W {
         self.write(buf).map_err(|e| Error::from(e))
     }
 }
+
+#[cfg(not(any(feature = "std", test)))]
+extern crate alloc;
+#[cfg(not(any(feature = "std", test)))]
+use alloc::vec::Vec;
+
+#[cfg(not(any(feature = "std", test)))]
+impl Write for Vec<u8> {
+    fn write(&mut self, buf: &[u8]) -> Result<usize, Error> {
+        self.extend_from_slice(buf);
+        Ok(buf.len())
+    }
+}
+
+#[cfg(not(any(feature = "std", test)))]
+impl Read for Vec<u8> {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, Error> {
+        if self.is_empty() {
+            return Err(Error::EndOfData);
+        }
+        let len = buf.len().min(self.len());
+        buf[..len].copy_from_slice(&self[..len]);
+        self.drain(..len);
+        Ok(len)
+    }
+}
