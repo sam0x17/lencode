d1ddab9aa4aef6e7845b3cd4acaf5d98b3381e46
Sam Johnson <sam@durosoft.com>
2025-06-09 19:18:10 -0400

    signed int skeleton

--- src/varint.rs
+++ src/varint.rs
@@ -28,6 +28,10 @@ pub trait Min {
     const MIN_VALUE: Self;
 }
 
+pub trait ByteLength {
+    const BYTE_LENGTH: usize;
+}
+
 pub trait UnsignedInteger:
     Sized
     + Copy
@@ -57,6 +61,7 @@ pub trait UnsignedInteger:
     + Zero
     + Max
     + Min
+    + ByteLength
 {
     fn encode_uint<S: Scheme>(self, writer: impl Write) -> Result<usize> {
         S::encode(self, writer)
@@ -82,9 +87,88 @@ macro_rules! impl_unsigned_integer {
             impl $crate::varint::Min for $t {
                 const MIN_VALUE: Self = <$t>::MIN;
             }
+            impl $crate::varint::ByteLength for $t {
+                const BYTE_LENGTH: usize = core::mem::size_of::<$t>();
+            }
             impl $crate::varint::UnsignedInteger for $t {}
         )*
     };
 }
 
 impl_unsigned_integer!(u8, u16, u32, u64, u128, usize);
+
+pub const fn zigzag_encode<I: SignedInteger>(value: I) -> I {
+    todo!()
+}
+
+pub const fn zigzag_decode<I: SignedInteger>(value: I) -> I {
+    todo!()
+}
+
+pub trait SignedInteger:
+    Sized
+    + Copy
+    + PartialEq
+    + Eq
+    + Debug
+    + Display
+    + Default
+    + Shl
+    + ShlAssign
+    + Shr
+    + ShrAssign
+    + BitAnd
+    + BitAndAssign
+    + BitOr
+    + BitOrAssign
+    + Add
+    + AddAssign
+    + Sub
+    + SubAssign
+    + Mul
+    + MulAssign
+    + Div
+    + DivAssign
+    + Neg
+    + Endianness
+    + One
+    + Zero
+    + Max
+    + Min
+    + ByteLength
+{
+    fn encode_int<S: Scheme>(self, writer: impl Write) -> Result<usize> {
+        todo!()
+        // S::encode(self, writer)
+    }
+    fn decode_int<S: Scheme>(reader: impl Read) -> Result<Self> {
+        todo!()
+        // S::decode(reader)
+    }
+}
+
+#[macro_export]
+macro_rules! impl_signed_integer {
+    ($($t:ty),*) => {
+        $(
+            impl $crate::varint::One for $t {
+                const ONE: Self = 1;
+            }
+            impl $crate::varint::Zero for $t {
+                const ZERO: Self = 0;
+            }
+            impl $crate::varint::Max for $t {
+                const MAX_VALUE: Self = <$t>::MAX;
+            }
+            impl $crate::varint::Min for $t {
+                const MIN_VALUE: Self = <$t>::MIN;
+            }
+            impl $crate::varint::ByteLength for $t {
+                const BYTE_LENGTH: usize = core::mem::size_of::<$t>();
+            }
+            impl $crate::varint::SignedInteger for $t {}
+        )*
+    };
+}
+
+impl_signed_integer!(i8, i16, i32, i64, i128, isize);
