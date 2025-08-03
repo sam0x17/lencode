ef060125df6b114ef2e529bea91763288a42ccd7
Sam Johnson <sam@durosoft.com>
2025-08-03 01:01:30 -0400

    fix benchmarks

--- benches/encode_decode.rs
+++ benches/encode_decode.rs
@@ -7,26 +7,24 @@ fn bench_encode(c: &mut Criterion) {
     c.bench_function("lencode_encode_u64", |b| {
         b.iter(|| {
             let mut cursor = Cursor::new(&mut buf[..]);
-            let value: u64 = black_box(0xFEDC_BA98_7654_3210);
+            let value: u64 = rand::random();
             black_box(Lencode::encode_varint(value, &mut cursor).unwrap());
         })
     });
 }
 
-fn bench_decode(c: &mut Criterion) {
+fn bench_round_trip(c: &mut Criterion) {
     let mut buf = [0u8; 16];
-    let value: u64 = 0xFEDC_BA98_7654_3210;
-    let n = {
-        let mut cursor = Cursor::new(&mut buf[..]);
-        Lencode::encode_varint(value, &mut cursor).unwrap()
-    };
-    c.bench_function("lencode_decode_u64", |b| {
+    c.bench_function("lencode_round_trip_u64", |b| {
         b.iter(|| {
+            let mut cursor = Cursor::new(&mut buf[..]);
+            let value: u64 = rand::random();
+            let n = Lencode::encode_varint(value, &mut cursor).unwrap();
             let mut cursor = Cursor::new(&buf[..n]);
             black_box(Lencode::decode_varint::<u64>(&mut cursor).unwrap());
         })
     });
 }
 
-criterion_group!(benches, bench_encode, bench_decode);
+criterion_group!(benches, bench_encode, bench_round_trip);
 criterion_main!(benches);
