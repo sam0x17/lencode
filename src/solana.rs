bdfd6862a0680092cd0ce1d78e07aecb3d4c0ab4
Sam Johnson <sam@durosoft.com>
2025-08-20 01:45:38 -0400

    encode MessageHeader as a varint encoded u32

--- src/solana.rs
+++ src/solana.rs
@@ -1,5 +1,6 @@
 use solana_sdk::{
     hash::{HASH_BYTES, Hash},
+    message::MessageHeader,
     pubkey::Pubkey,
     signature::{SIGNATURE_BYTES, Signature},
     transaction::SanitizedTransaction,
@@ -49,6 +50,30 @@ impl Decode for Signature {
     }
 }
 
+impl Encode for MessageHeader {
+    fn encode(&self, writer: &mut impl Write) -> Result<usize> {
+        let combined = u32::from_le_bytes([
+            self.num_required_signatures,
+            self.num_readonly_signed_accounts,
+            self.num_readonly_unsigned_accounts,
+            0,
+        ]);
+        combined.encode(writer)
+    }
+}
+
+impl Decode for MessageHeader {
+    fn decode(reader: &mut impl Read) -> Result<Self> {
+        let combined: u32 = decode(reader)?;
+        let combined_bytes = combined.to_le_bytes();
+        Ok(MessageHeader {
+            num_required_signatures: combined_bytes[0],
+            num_readonly_signed_accounts: combined_bytes[1],
+            num_readonly_unsigned_accounts: combined_bytes[2],
+        })
+    }
+}
+
 impl Encode for SanitizedTransaction {
     #[inline(always)]
     fn encode(&self, writer: &mut impl Write) -> Result<usize> {
@@ -67,4 +92,31 @@ fn test_encode_decode_pubkey() {
         let decoded_pubkey = Pubkey::decode(&mut Cursor::new(&mut buf)).unwrap();
         assert_eq!(pubkey, decoded_pubkey);
     }
+}
+
+#[test]
+fn test_encode_decode_message_header() {
+    for _ in 0..1000 {
+        let header = MessageHeader {
+            num_required_signatures: rand::random::<u8>(),
+            num_readonly_signed_accounts: rand::random::<u8>(),
+            num_readonly_unsigned_accounts: rand::random::<u8>(),
+        };
+        let mut buf = [0u8; 4];
+        let mut cursor = Cursor::new(&mut buf);
+        header.encode(&mut cursor).unwrap();
+        let decoded_header = MessageHeader::decode(&mut Cursor::new(&mut buf)).unwrap();
+        assert_eq!(header, decoded_header);
+    }
+    let header = MessageHeader {
+        num_required_signatures: 0,
+        num_readonly_signed_accounts: 0,
+        num_readonly_unsigned_accounts: 0,
+    };
+    let mut buf = [0u8; 4];
+    let mut cursor = Cursor::new(&mut buf);
+    let n = header.encode(&mut cursor).unwrap();
+    assert_eq!(n, 1);
+    let decoded_header = MessageHeader::decode(&mut Cursor::new(&mut buf)).unwrap();
+    assert_eq!(header, decoded_header);
 }
