7e53587177d7ad0d9ccde70a2900612ed7953de5
Sam Johnson <sam@durosoft.com>
2025-08-22 02:00:18 -0400

    fix

--- src/dedupe.rs
+++ src/dedupe.rs
@@ -4,32 +4,23 @@ use hashbrown::{DefaultHashBuilder, HashTable, hash_table::Entry};
 
 use crate::prelude::*;
 
-pub struct DedupeEncoder {
-    table: HashTable<usize>,
-}
-
 const FIRST_OCCURRENCE_ID: usize = 0;
 
-impl DedupeEncoder {
-    #[inline(always)]
-    pub const fn new() -> Self {
-        DedupeEncoder {
-            table: HashTable::new(),
-        }
-    }
+#[cfg(feature = "std")]
+thread_local! {
+    static TABLE: std::cell::RefCell<HashTable<usize>> = std::cell::RefCell::new(HashTable::new());
+}
+#[cfg(not(feature = "std"))]
+static TABLE: critical_section::Mutex<core::cell::RefCell<HashTable<usize>>> =
+    critical_section::Mutex::new(core::cell::RefCell::new(HashTable::new()));
 
-    #[inline]
-    pub fn encode<T: Hash + Eq + Encode>(
-        &mut self,
-        val: T,
-        writer: &mut impl Write,
-    ) -> Result<usize> {
-        let hashcode = DefaultHashBuilder::default().hash_one(&val);
-        let id = self.table.len() + 1; // 0 is reserved for the first occurrence of new values
-        match self
-            .table
-            .entry(hashcode, |&stored_index| stored_index == id, |&_| hashcode)
-        {
+#[inline]
+#[cfg(feature = "std")]
+pub fn encode_dedupe<T: Hash + Eq + Encode>(val: T, writer: &mut impl Write) -> Result<usize> {
+    let hashcode = DefaultHashBuilder::default().hash_one(&val);
+    TABLE.with_borrow_mut(|table| {
+        let id = table.len() + 1; // 0 is reserved for the first occurrence of new values
+        match table.entry(hashcode, |&stored_index| stored_index == id, |&_| hashcode) {
             Entry::Occupied(entry) => Lencode::encode_varint(*entry.get(), writer),
             Entry::Vacant(entry) => {
                 entry.insert(id);
@@ -37,5 +28,26 @@ impl DedupeEncoder {
                 val.encode(writer)
             }
         }
-    }
+    })
 }
+
+#[inline]
+#[cfg(not(feature = "std"))]
+pub fn encode_dedupe<T: Hash + Eq + Encode>(val: T, writer: &mut impl Write) -> Result<usize> {
+    let hashcode = DefaultHashBuilder::default().hash_one(&val);
+    critical_section::with(|cs| {
+        let table = TABLE.borrow(cs);
+        let mut table = table.borrow_mut();
+        let id = table.len() + 1; // 0 is reserved for the first occurrence of new values
+        match table.entry(hashcode, |&stored_index| stored_index == id, |&_| hashcode) {
+            Entry::Occupied(entry) => Lencode::encode_varint(*entry.get(), writer),
+            Entry::Vacant(entry) => {
+                entry.insert(id);
+                Lencode::encode_varint(FIRST_OCCURRENCE_ID, writer)?;
+                val.encode(writer)
+            }
+        }
+    })
+}
+
+// TODO: actually compare values with Eq
