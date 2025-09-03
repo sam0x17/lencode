47e4e8b38b0963796385fecb805d46f0e9b6bbd5
Sam Johnson <sam@durosoft.com>
2025-09-03 00:59:12 -0400

    fix

--- examples/size_comparison.rs
+++ examples/size_comparison.rs
@@ -27,7 +27,7 @@ fn main() {
     let borsh_data = borsh::to_vec(&all_pubkeys).unwrap();
 
     // Encode with lencode + deduplication
-    let mut encoder = DedupeEncoder::with_capacity(1000);
+    let mut encoder = DedupeEncoder::with_capacity(1000, 1);
     let mut cursor = Cursor::new(Vec::new());
     all_pubkeys.encode(&mut cursor, Some(&mut encoder)).unwrap();
     let lencode_data = cursor.into_inner();
