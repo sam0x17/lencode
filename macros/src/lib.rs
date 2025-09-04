07909c193523476c5090deaeed3f2260c46c63db
Sam Johnson <sam@durosoft.com>
2025-09-04 12:19:47 -0400

    enum support

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -74,10 +74,80 @@ fn derive_encode_impl(input: impl Into<TokenStream2>) -> Result<TokenStream2> {
                 }
             });
         }
-        syn::Data::Enum(data_enum) => todo!(),
-        syn::Data::Union(data_union) => todo!(),
+        syn::Data::Enum(data_enum) => {
+            let name = derive_input.ident;
+            let variant_matches = data_enum.variants.iter().enumerate().map(|(idx, v)| {
+				let vname = &v.ident;
+				let idx_lit = syn::Index::from(idx);
+				match &v.fields {
+					syn::Fields::Named(named_fields) => {
+						let field_names: Vec<_> = named_fields.named.iter().map(|f| f.ident.as_ref().unwrap()).collect();
+						let field_encodes = field_names.iter().map(|fname| {
+							quote! {
+								total_bytes += #fname.encode_ext(writer, dedupe_encoder.as_deref_mut())?;
+							}
+						});
+						quote! {
+							#name::#vname { #(#field_names),* } => {
+								total_bytes += (#idx_lit as u64).encode_ext(writer, dedupe_encoder.as_deref_mut())?;
+								#(#field_encodes)*
+							}
+						}
+					}
+					syn::Fields::Unnamed(unnamed_fields) => {
+						let field_indices: Vec<syn::Ident> = (0..unnamed_fields.unnamed.len())
+							.map(|i| Ident::new(&format!("field{}", i), Span::call_site()))
+							.collect();
+						let field_encodes = field_indices.iter().map(|fname| {
+							quote! {
+								total_bytes += #fname.encode_ext(writer, dedupe_encoder.as_deref_mut())?;
+							}
+						});
+						quote! {
+							#name::#vname( #(#field_indices),* ) => {
+								total_bytes += (#idx_lit as u64).encode_ext(writer, dedupe_encoder.as_deref_mut())?;
+								#(#field_encodes)*
+							}
+						}
+					}
+					syn::Fields::Unit => {
+						quote! {
+							#name::#vname => {
+								total_bytes += (#idx_lit as u64).encode_ext(writer, dedupe_encoder.as_deref_mut())?;
+							}
+						}
+					}
+				}
+			});
+            return Ok(quote! {
+                {
+                    use #krate::prelude::*;
+                    impl #krate::prelude::Encode for #name {
+
+                        #[inline(always)]
+                        fn encode_ext(
+                            &self,
+                            writer: &mut impl Write,
+                            mut dedupe_encoder: Option<&mut DedupeEncoder>,
+                        ) -> Result<usize> {
+                            let mut total_bytes = 0;
+                            match self {
+                                #(#variant_matches)*
+                            }
+                            Ok(total_bytes)
+                        }
+                    }
+                }
+            });
+        }
+        syn::Data::Union(_data_union) => {
+            // Unions are not supported
+            return Err(syn::Error::new_spanned(
+                derive_input.ident,
+                "Encode cannot be derived for unions",
+            ));
+        }
     }
-    todo!()
 }
 
 #[test]
@@ -89,6 +159,21 @@ fn test_derive_encode_struct_basic() {
         }
     };
     let derived = derive_encode_impl(tokens).unwrap();
-    let expected = quote! { hey };
+    let expected = quote! { {
+        use crate::prelude::*;
+        impl crate::prelude::Encode for TestStruct {
+            #[inline(always)]
+            fn encode_ext(
+                &self,
+                writer: &mut impl Write,
+                mut dedupe_encoder: Option<&mut DedupeEncoder>,
+            ) -> Result<usize> {
+                let mut total_bytes = 0;
+                total_bytes += self.a.encode_ext(writer, dedupe_encoder.as_deref_mut())?;
+                total_bytes += self.b.encode_ext(writer, dedupe_encoder.as_deref_mut())?;
+                Ok(total_bytes)
+            }
+        }
+    } };
     assert_eq!(derived.to_string(), expected.to_string());
 }
