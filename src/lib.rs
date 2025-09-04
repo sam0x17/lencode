cff5cbfcb8aae9736d645d251cae0358bfa4ef93
Sam Johnson <sam@durosoft.com>
2025-09-04 01:25:09 -0400

    implement Encode/Decode for core::ops::Range

--- src/lib.rs
+++ src/lib.rs
@@ -640,6 +640,38 @@ impl<V: Decode + Eq + std::hash::Hash> Decode for std::collections::HashSet<V> {
     }
 }
 
+impl<T: Encode> Encode for core::ops::Range<T> {
+    #[inline(always)]
+    fn encode_ext(
+        &self,
+        writer: &mut impl Write,
+        mut dedupe_encoder: Option<&mut crate::dedupe::DedupeEncoder>,
+    ) -> Result<usize> {
+        let mut total_written = 0;
+        total_written += self
+            .start
+            .encode_ext(writer, dedupe_encoder.as_deref_mut())?;
+        total_written += self.end.encode_ext(writer, dedupe_encoder.as_deref_mut())?;
+        Ok(total_written)
+    }
+}
+
+impl<T: Decode> Decode for core::ops::Range<T> {
+    #[inline(always)]
+    fn decode_ext(
+        reader: &mut impl Read,
+        mut dedupe_decoder: Option<&mut crate::dedupe::DedupeDecoder>,
+    ) -> Result<Self> {
+        let start = T::decode_ext(reader, dedupe_decoder.as_deref_mut())?;
+        let end = T::decode_ext(reader, dedupe_decoder.as_deref_mut())?;
+        Ok(core::ops::Range { start, end })
+    }
+
+    fn decode_len(_reader: &mut impl Read) -> Result<usize> {
+        unimplemented!()
+    }
+}
+
 #[test]
 fn test_encode_decode_i16_all() {
     for i in i16::MIN..=i16::MAX {
