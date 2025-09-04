ba65f53dcf22ac5b7af5844b2d1c8d9a1c033cb9
Sam Johnson <sam@durosoft.com>
2025-09-04 12:44:19 -0400

    almost working

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -22,6 +22,14 @@ pub fn derive_encode(input: TokenStream) -> TokenStream {
     }
 }
 
+#[proc_macro_derive(Decode)]
+pub fn derive_decode(input: TokenStream) -> TokenStream {
+    match derive_decode_impl(input) {
+        Ok(ts) => ts.into(),
+        Err(err) => err.to_compile_error().into(),
+    }
+}
+
 #[inline(always)]
 fn derive_encode_impl(input: impl Into<TokenStream2>) -> Result<TokenStream2> {
     let derive_input = parse2::<DeriveInput>(input.into())?;
@@ -150,6 +158,126 @@ fn derive_encode_impl(input: impl Into<TokenStream2>) -> Result<TokenStream2> {
     }
 }
 
+#[inline(always)]
+fn derive_decode_impl(input: impl Into<TokenStream2>) -> Result<TokenStream2> {
+    let derive_input = parse2::<DeriveInput>(input.into())?;
+    let krate = crate_path();
+    match derive_input.data {
+        syn::Data::Struct(data_struct) => {
+            let name = derive_input.ident;
+            let fields = data_struct.fields;
+            let decode_body = match fields {
+                syn::Fields::Named(ref named_fields) => {
+                    let field_decodes = named_fields.named.iter().map(|f| {
+                        let fname = &f.ident;
+                        let ftype = &f.ty;
+                        quote! {
+                            #fname: <#ftype>::decode_ext(reader, dedupe_decoder.as_deref_mut())?,
+                        }
+                    });
+                    quote! {
+                        Ok(#name {
+                            #(#field_decodes)*
+                        })
+                    }
+                }
+                syn::Fields::Unnamed(ref unnamed_fields) => {
+                    let field_decodes = unnamed_fields.unnamed.iter().map(|f| {
+                        let ftype = &f.ty;
+                        quote! {
+                            <#ftype>::decode_ext(reader, dedupe_decoder.as_deref_mut())?,
+                        }
+                    });
+                    quote! {
+                        Ok(#name(
+                            #(#field_decodes)*
+                        ))
+                    }
+                }
+                syn::Fields::Unit => quote! { Ok(#name) },
+            };
+            return Ok(quote! {
+                {
+                    use #krate::prelude::*;
+                    impl #krate::prelude::Decode for #name {
+
+                        #[inline(always)]
+                        fn decode_ext(
+                            reader: &mut impl Read,
+                            mut dedupe_decoder: Option<&mut DedupeDecoder>,
+                        ) -> Result<Self> {
+                            #decode_body
+                        }
+                    }
+                }
+            });
+        }
+        syn::Data::Enum(data_enum) => {
+            let name = derive_input.ident;
+            let variant_matches = data_enum.variants.iter().enumerate().map(|(idx, v)| {
+                let vname = &v.ident;
+                let idx_lit = syn::Index::from(idx);
+                match &v.fields {
+                    syn::Fields::Named(named_fields) => {
+                        let field_decodes = named_fields.named.iter().map(|f| {
+                            let fname = &f.ident;
+                            let ftype = &f.ty;
+							quote! {
+								#fname: <#ftype>::decode_ext(reader, dedupe_decoder.as_deref_mut())?,
+							}
+						});
+                        quote! {
+                            #idx_lit => Ok(#name::#vname { #(#field_decodes)* }),
+                        }
+                    }
+                    syn::Fields::Unnamed(unnamed_fields) => {
+                        let field_decodes = unnamed_fields.unnamed.iter().map(|f| {
+                            let ftype = &f.ty;
+                            quote! {
+                                <#ftype>::decode_ext(reader, dedupe_decoder.as_deref_mut())?,
+                            }
+                        });
+                        quote! {
+                            #idx_lit => Ok(#name::#vname( #(#field_decodes)* )),
+                        }
+                    }
+                    syn::Fields::Unit => {
+                        quote! {
+                            #idx_lit => Ok(#name::#vname),
+                        }
+                    }
+                }
+            });
+            return Ok(quote! {
+                {
+                    use #krate::prelude::*;
+                    impl #krate::prelude::Decode for #name {
+
+                        #[inline(always)]
+                        fn decode_ext(
+                            reader: &mut impl Read,
+                            mut dedupe_decoder: Option<&mut DedupeDecoder>,
+                        ) -> Result<Self> {
+                            let variant_idx = u64::decode_ext(reader, dedupe_decoder.as_deref_mut())? as usize;
+                            match variant_idx {
+                                #(#variant_matches)*
+                                _ => Err(#krate::io::Error::InvalidData),
+                            }
+                        }
+                    }
+                }
+            });
+        }
+        syn::Data::Union(_data_union) => {
+            // Unions are not supported
+            return Err(syn::Error::new_spanned(
+                derive_input.ident,
+                "Decode cannot be derived for unions",
+            ));
+        }
+    }
+}
+
 #[test]
 fn test_derive_encode_struct_basic() {
     let tokens = quote! {
@@ -172,6 +300,33 @@ fn test_derive_encode_struct_basic() {
                 total_bytes += self.a.encode_ext(writer, dedupe_encoder.as_deref_mut())?;
                 total_bytes += self.b.encode_ext(writer, dedupe_encoder.as_deref_mut())?;
                 Ok(total_bytes)
+            }
+        }
+    } };
+    assert_eq!(derived.to_string(), expected.to_string());
+}
+
+#[test]
+fn test_derive_decode_struct_basic() {
+    let tokens = quote! {
+        struct TestStruct {
+            a: u32,
+            b: String,
+        }
+    };
+    let derived = derive_decode_impl(tokens).unwrap();
+    let expected = quote! { {
+        use crate::prelude::*;
+        impl crate::prelude::Decode for TestStruct {
+            #[inline(always)]
+            fn decode_ext(
+                reader: &mut impl Read,
+                mut dedupe_decoder: Option<&mut DedupeDecoder>,
+            ) -> Result<Self> {
+                Ok(TestStruct {
+                    a: <u32>::decode_ext(reader, dedupe_decoder.as_deref_mut())?,
+                    b: <String>::decode_ext(reader, dedupe_decoder.as_deref_mut())?,
+                })
             }
         }
     } };
--- tests/derive_tests.rs
+++ tests/derive_tests.rs
@@ -1,15 +1,101 @@
-use lencode::prelude::Encode;
+use lencode::prelude::*;
 
-#[derive(Encode)]
+#[derive(Encode, Decode, Debug, PartialEq)]
 pub struct Foo {
     pub a: u128,
     pub b: bool,
     pub c: [u64; 18],
 }
 
-#[derive(Encode)]
+#[derive(Encode, Decode, Debug, PartialEq)]
 pub enum Bar {
     A(u32),
     B { x: String, y: Vec<u8> },
     C,
+}
+
+#[test]
+fn test_struct_encode_decode_roundtrip() {
+    let original = Foo {
+        a: 12345,
+        b: true,
+        c: [
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+        ],
+    };
+
+    let mut buffer = Vec::new();
+    let bytes_written = original.encode(&mut buffer).unwrap();
+    assert!(bytes_written > 0);
+
+    let mut cursor = Cursor::new(&buffer);
+    let decoded: Foo = Foo::decode(&mut cursor).unwrap();
+
+    assert_eq!(original, decoded);
+}
+
+#[test]
+fn test_enum_encode_decode_roundtrip() {
+    let test_cases = vec![
+        Bar::A(42),
+        Bar::B {
+            x: "test".to_string(),
+            y: vec![1, 2, 3, 4, 5],
+        },
+        Bar::C,
+    ];
+
+    for original in test_cases {
+        let mut buffer = Vec::new();
+        let bytes_written = original.encode(&mut buffer).unwrap();
+        assert!(bytes_written > 0);
+
+        let mut cursor = Cursor::new(&buffer);
+        let decoded: Bar = Bar::decode(&mut cursor).unwrap();
+
+        assert_eq!(original, decoded);
+    }
+}
+
+#[test]
+fn test_struct_with_deduplication() {
+    let original = Foo {
+        a: 9876543210,
+        b: false,
+        c: [100; 18], // All the same value for testing
+    };
+
+    let mut buffer = Vec::new();
+    let mut encoder = DedupeEncoder::new();
+    let bytes_written = original
+        .encode_ext(&mut buffer, Some(&mut encoder))
+        .unwrap();
+    assert!(bytes_written > 0);
+
+    let mut cursor = Cursor::new(&buffer);
+    let mut decoder = DedupeDecoder::new();
+    let decoded: Foo = Foo::decode_ext(&mut cursor, Some(&mut decoder)).unwrap();
+
+    assert_eq!(original, decoded);
+}
+
+#[test]
+fn test_enum_with_deduplication() {
+    let original = Bar::B {
+        x: "hello world".to_string(),
+        y: vec![255; 10],
+    };
+
+    let mut buffer = Vec::new();
+    let mut encoder = DedupeEncoder::new();
+    let bytes_written = original
+        .encode_ext(&mut buffer, Some(&mut encoder))
+        .unwrap();
+    assert!(bytes_written > 0);
+
+    let mut cursor = Cursor::new(&buffer);
+    let mut decoder = DedupeDecoder::new();
+    let decoded: Bar = Bar::decode_ext(&mut cursor, Some(&mut decoder)).unwrap();
+
+    assert_eq!(original, decoded);
 }
