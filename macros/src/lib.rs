516e12acd12b951ff2e55607abec584f91a70cdc
Sam Johnson <sam@durosoft.com>
2025-09-04 02:10:16 -0400

    derive_encode scaffold

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,1 +1,23 @@
+use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
+use syn::{DeriveInput, Result, parse2};
 
+#[proc_macro_derive(Encode)]
+pub fn derive_encode(input: TokenStream) -> TokenStream {
+    match derive_encode_impl(input) {
+        Ok(ts) => ts.into(),
+        Err(err) => err.to_compile_error().into(),
+    }
+}
+
+#[inline(always)]
+fn derive_encode_impl(input: impl Into<TokenStream2>) -> Result<TokenStream2> {
+    let derive_input = parse2::<DeriveInput>(input.into())?;
+    match derive_input.data {
+        syn::Data::Struct(data_struct) => todo!(),
+        syn::Data::Enum(data_enum) => todo!(),
+        syn::Data::Union(data_union) => todo!(),
+    }
+    todo!()
+}
