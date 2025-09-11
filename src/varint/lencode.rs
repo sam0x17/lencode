1f2b1f63d872832c461624d81c2b402c0351f01b
Sam Johnson <sam@durosoft.com>
2025-09-11 01:40:13 -0400

    fix

--- src/varint/lencode.rs
+++ src/varint/lencode.rs
@@ -61,7 +61,7 @@ impl VarintEncodingScheme for Lencode {
             // }
             reader.read(&mut val_bytes[..n])?;
             #[cfg(target_endian = "big")]
-            endian_cast::reverse(val_bytes);
+            val_bytes[..n].reverse();
             Ok(val)
         }
     }
