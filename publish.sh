02be30bd14b42958e17ee5bce656d29cc688cdf0
Sam Johnson <sam@durosoft.com>
2025-09-16 10:57:30 -0400

    fix

--- publish.sh
+++ publish.sh
@@ -4,8 +4,6 @@ cargo check --workspace
 cargo check --workspace --all-features
 cargo test --workspace
 cargo test --workspace --all-features
-cargo bench --check
-cargo bench --all-features --check
 cargo doc --workspace
 cargo doc --workspace --all-features
 cd macros
