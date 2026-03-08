4a77103c73ed6b178e12c67030d34b3f6b2dabb3
Sam Johnson <sam@durosoft.com>
2026-03-08 03:24:27 -0400

    clean up more

--- comparison_u64.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-LENCODE_CODEC_FILTER=regular_u64 \
-  cargo bench --color=always --features "std,comparison-bench" \
-  --bench codec_bench -- regular_u64 2>&1 | grep -E 'rank|\[size\]|error|warning'
-
-cargo bench --color=always --features "std,comparison-bench" \
-  --bench varint_bench -- u64 2>&1 | grep -E 'rank|\[size\]|error|warning'
